4320d9f3ecf36dec5eaafb65aed74b0e93ca4a3b
[Darcy Winterhill] <[darcy.winterhill@gmail.com]>
2023-11-08 11:30:08 +0100

    Add project files.

---
+++ Checkpoint2/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkpoint2
+{
+    internal class Product
+    {
+        public string Category {  get; set; }
+        public string ProductName {  get; set; }
+        public int Price {  get; set; }
+
+        public string Print()
+        {
+            return Category.PadRight(10) + ProductName.PadRight(10) + Price;
+        }
+    }
+
+}
---
+++ Checkpoint2/Program.cs
@@ -0,0 +1,46 @@
+﻿
+using Checkpoint2;
+
+List<Product> productList = new List<Product>();
+
+while (true)
+{
+    Console.WriteLine("-------------------------------------------------------------------------");
+    Console.ForegroundColor = ConsoleColor.DarkYellow;
+    Console.WriteLine("Registrera en produkt genom att följa stegen | Avsluta genom att skriva Q");
+    Console.ResetColor();
+
+    Console.Write("Kategori: ");
+    string category = Console.ReadLine();
+
+    if (category.ToLower().Trim() == "q")
+    {
+        break;
+    }
+
+    Console.Write("Produktnamn: ");
+    string productName = Console.ReadLine();
+
+    Console.Write("Pris (heltal): ");
+    int price;
+    while (!int.TryParse(Console.ReadLine(), out price))
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Ange pris (heltal): ");
+        Console.ResetColor();
+    }
+
+    Product newProduct = new Product
+    {
+        Category = category,
+        ProductName = productName,
+        Price = price
+    };
+
+    productList.Add(newProduct);
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Produkten lades till");
+    Console.ResetColor();
+}
+
+Console.ReadLine();
